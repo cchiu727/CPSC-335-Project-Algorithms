b8c9168744aa365c852c861f1d2d52a9c21432ae
Collin Chiu <cchiu727@gmail.com>
2022-03-09 17:46:00 -0800

    Re-organized and cleaned up main branch

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,21 +1,67 @@
-/***********************************************
-
-	Class DoublyList
-
-************************************************/
-
-#include "DoublyList.h"
-
+// Contributors: Collin Chiu, Brian Joh, Stephanie Beccera
+// File name: 
 #include <iostream>
-using namespace std;
+#include <list>
 
+std::list<int> circleList;
+
+// printing the list
+void printList() {
+    for (int n : circleList) {
+        std::cout << n << ", ";
+    }
+    std::cout << std::endl;
+}
+
+// generating the list
+void genList() {
+    int num;
+    std::cout << "Enter the number of items in the list (n > 0): ";
+    std::cin >> num;
+
+    int data;
+
+    for (int i = 0; i < (2 * num); i++) {
+        if (i % 2 == 0)
+            data = 0;
+        else
+            data = 1;
+        circleList.push_front(data);
+    }
+
+    std::cout << "List: \t ";
+    printList();
+}
+
+// sorts list so all 0's are on left side, all 1's are on right side
+void sortList() {
+    int swapCount = 0;
+
+    std::list<int>::iterator it1, it2;
+    
+    for (int loopCounter = 0; loopCounter < circleList.size(); loopCounter++) {
+        for (it1 = circleList.begin(), it2 = it1++; it2 != circleList.end(); it1++, it2++) {
+            if (*it1 == 0 && *it2 == 1) {
+                std::swap(*it1, *it2);
+                swapCount++;
+
+                // output formatting
+                std::cout << "Swapped: ";
+                printList();
+            }
+            else {}
+        }
+    }
+
+    std::cout << "Number of swaps: " << swapCount << std::endl;
+}
+
+// main
 int main()
 {
-	DoublyList dList;
+    genList();
 
-	dList.createAList();
+    sortList();
 
-	cout << endl << endl;
-	system("Pause");
-	return 0;
+    return 0;
 }
---
+++ Project1/PROJECT_ONE_README.md
@@ -0,0 +1,20 @@
+# 335ProjectOne
+#Project Group Members: Collin Chiu, Stephanie Becerra, Brian Joh
+
+#Introduction
+This project is to solve a problem using algorithms. This specific project gives us an array of circles that alternate from light, dark, light, dark, etc.
+Using algorithms, we need to organize the circles so that all the dark circles are on the right side with the light circles on the left. The input of the
+algorithm should be a positive integer 𝑛 and a list of 2𝑛 disks of alternating colors light-dark, starting with light and the output should be a list of 2𝑛 
+disks, the first 𝑛 disks are dark, the next 𝑛 disks are light, and an integer 𝑚 representing the number of swaps to move the light ones after the dark ones.
+
+#Important Note
+Since C++ cannot make actual shapes in code, we used numbers to represent the light and dark circles. 0 represents the dark circles and 1 represents the light circles
+
+#Requirements
+The only thing you need is the cpp files themselves. Nothing extra is needed other than basic Linux commands.
+
+#Installation
+Install this code by simply downloading it and running it normally in Tuffix. The commands for running are g++ main.cpp -o main.cpp and ./main
+
+#Configuration
+You are allowed to determine how many items appear in the list.
