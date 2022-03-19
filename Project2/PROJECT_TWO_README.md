7bf7800f73e797b3ce9fdcc1c42a6e68227322d7
Stephanie Becerra <stephanie.becerra.2018@gmail.com>
2022-03-18 19:46:31 -0700

    Added part 1 a working code
    This runs a for loop that does the algorithm for part a to print the fibonacci sequence up to the 15th place

---
+++ Project2/PROJECT_TWO_README.md
@@ -0,0 +1,6 @@
+Project 2
+
+Contributors: Collin Chiu, Brian Joh, Stephanie Beccera
+
+Supposed to solve the Fibonacci sequence problem in 2 different way. Exhaustive pattern and The Golden Ratio Approach.
+Then haveing a set of problems due The Largest Sum Subarray Problem 
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,10 +3,38 @@
 #include <iostream>
 #include <list>
 
+using namespace std;
+
+
+void exhaustivePattern()
+{
+    int n = 15, t1 = 0, t2 = 1, nextTerm = 0; 
+    cout << "Fibonacci Series with exhaustive Pattern: ";
+    for (int i = 1; i <= n; ++i) 
+    {
+        // Prints the first two terms.
+        if (i == 1) 
+        {
+            cout << t1 << ", ";
+            continue;
+        }
+        if (i == 2) 
+        {
+            cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+
+        cout << nextTerm << ", ";
+    }
+    cout << endl;
+}
 // main
 int main()
 {
-   
+    exhaustivePattern();
 
     return 0;
 }
