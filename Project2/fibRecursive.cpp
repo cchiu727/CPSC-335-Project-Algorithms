36f7d29e137a1df336f0f195878006b6448407e9
Collin Chiu <cchiu727@gmail.com>
2022-03-21 15:16:00 -0700

    Completed Problem 1A of project, will double check

---
+++ Project2/fibRecursive.cpp
@@ -0,0 +1,40 @@
+// Contributors: Stephanie Beccera, Collin Chiu, Brian Joh
+// File name: fibRecursive.cpp
+// Project 2 Part 1A
+
+#include <iostream>
+#include <vector>
+
+// recursive function to generate fib series
+// argument n is the number of terms in the series
+// returns n if n is 0 or 1, or returns next number in series
+int fib(int n) {
+    if (n == 0 || n == 1)
+        return n;
+    else
+        return (fib(n - 1) + fib (n - 2));
+}
+
+// user prompt to ask user number of terms in the series
+int userPrompt() {
+    int num;
+    std::cout << "Enter the number of terms in the Fibonacci series." << std::endl;
+    std::cout << "(Must be an integer n > 0):";
+    std::cin >> num;
+    if (num < 0) {
+        std::cout << "ERROR: Invalid input" << std::endl;
+        userPrompt();
+    }
+    
+    return num;
+}
+
+int main() {
+    int num = userPrompt();
+
+    for (int i = 0; i < num; i++) {
+        std::cout << fib(i) << ", ";
+    }
+
+    return 0;
+}
